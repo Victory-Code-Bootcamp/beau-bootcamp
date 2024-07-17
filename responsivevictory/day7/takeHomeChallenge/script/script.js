135e849be7114f6a56c5e8102459e8be31dc8def
beau currier <beaucurrier01@gmail.com>
2024-07-16 20:10:12 -0700

    changed some wording

--- responsivevictory/day7/takeHomeChallenge/script/script.js
+++ responsivevictory/day7/takeHomeChallenge/script/script.js
@@ -4,10 +4,10 @@ let childNum = 35;
 let partner = 'a lady';
 let location = 'the moon';
 
-console.log(`You will be ${job} on ${location}, and married to ${partner} with ${childNum} kids.`)
+console.log(`You will be ${job} on ${location}, married to ${partner} with ${childNum} kids, and be ${age} years old`)
 
 let birthYear = 1977;
 let futureYear = 2030;
 let ageInFuture = futureYear - birthYear;
-let ageInFuture2 = futureYear - birthYear +1;
-console.log(`You will be ${ageInFuture} or ${ageInFuture2} years old in ${futureYear}.`)
+let ageInFuture2 = futureYear - birthYear -1;
+console.log(`You will be ${ageInFuture2} or ${ageInFuture} years old in ${futureYear}.`)
