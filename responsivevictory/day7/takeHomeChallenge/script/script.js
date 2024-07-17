cf19594bd2e6ef41686d963d83c2a26eca048ca6
beau currier <beaucurrier01@gmail.com>
2024-07-16 20:03:40 -0700

    fun with js

---
+++ responsivevictory/day7/takeHomeChallenge/script/script.js
@@ -0,0 +1,13 @@
+let job = 'an astronaught';
+let age = 168;
+let childNum = 35;
+let partner = 'a lady';
+let location = 'the moon';
+
+console.log(`You will be ${job} on ${location}, and married to ${partner} with ${childNum} kids.`)
+
+let birthYear = 1977;
+let futureYear = 2030;
+let ageInFuture = futureYear - birthYear;
+let ageInFuture2 = futureYear - birthYear +1;
+console.log(`You will be ${ageInFuture} or ${ageInFuture2} years old in ${futureYear}.`)
