f14956b86cb616fe699d4ee200b9ea3f76971494
beau currier <beaucurrier01@gmail.com>
2024-07-18 22:13:19 -0700

    daily chaalange day 8

---
+++ responsivevictory/day8/dailyChallenge/script/script.js
@@ -0,0 +1,18 @@
+// CHALLENGE 1
+// let person = prompt ("enter name");
+// console.log(person.length)
+// alert(`Your name ${person} has ${person.length} characters.`)
+
+// CHALLENGE 2
+// let food = prompt ('favorite food')
+// let color = prompt ('favorite color')
+// alert (`Your favorite food is ${color.concat(food)}`)
+
+// CHALLENGE 3
+let kateString = 'this is my string';
+kateString = kateString.replace("t", "T");
+console.log(kateString);
+
+// CHALLENGE 4
+let maxString = 'Max is a Javascript programmer';
+console.log(maxString.includes('Javascript'))
