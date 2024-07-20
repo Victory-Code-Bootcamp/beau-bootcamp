278a7a99439cf2ff12e2aa0e0b6f7bb4dda4a829
beau currier <beaucurrier01@gmail.com>
2024-07-19 22:33:13 -0700

    day 8 take home challenge

---
+++ responsivevictory/day8/takeHomeChallenge/script/script.js
@@ -0,0 +1,16 @@
+const num1 = 7;
+const num2 = 5;
+const biggest = Math.max(num1, num2);
+console.log(biggest);
+
+let random = Math.random()*100;
+console.log(Math.floor(random));
+
+let numSquare = prompt('enter a number');
+console.log(numSquare*numSquare);
+console.log(Math.pow(numSquare,2));
+
+let date = new Date();
+console.log(date);
+console.log(date.getFullYear());
+console.log(date.getDate() + 10);
