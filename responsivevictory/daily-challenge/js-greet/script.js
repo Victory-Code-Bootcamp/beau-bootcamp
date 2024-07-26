50c6da49edb062c430e9726d8fcdc2dc9c039783
beau currier <beaucurrier01@gmail.com>
2024-07-25 23:00:50 -0700

    started daily challenge js-greet

---
+++ responsivevictory/daily-challenge/js-greet/script.js
@@ -0,0 +1,10 @@
+
+let hour = () => new Date().getHours()
+let greet = hour < 12 && hour === 7 ? 'Time for work' : hour < 12 ? "Good Morning" : hour > 12 && hour === 18? 'Dinner time!' : 'Good Evening'
+
+console.log(greet)
+
+let calculateDiscount = (num1, num2) => {
+    
+}
+
