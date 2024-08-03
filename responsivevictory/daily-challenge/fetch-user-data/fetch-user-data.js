42f07e6314c17ad5704fe153f18f130392dae234
beau currier <beaucurrier01@gmail.com>
2024-08-03 12:29:15 -0700

    finnished fetch-user-data

--- responsivevictory/daily-challenge/fetch-user-data/fetch-user-data.js
+++ responsivevictory/daily-challenge/fetch-user-data/fetch-user-data.js
@@ -1,3 +1,9 @@
+// Task 1: Create a function named fetchUserData using fetch and async/await to fetch data from https://jsonplaceholder.typicode.com/users and pass the data to a callback function.
+// Parameters of fetchUserData: callback (a function to process the fetched data)
+// Task 2: Create a function named extractAndLogUserData that takes user data as an argument, extracts the first three user names and emails, and logs them.
+// Parameters of extractAndLogUserData: users (an array of user objects)
+// Task 3: Pass extractAndLogUserData as an argument to fetchUserData and see the results
+
 let fetchUserData = async (callback) => {
     const response = await fetch('https://jsonplaceholder.typicode.com/users');
     const data = await response.json();
