22a502a0e635e90f5584b5828bc9e333a4966854
beau currier <beaucurrier01@gmail.com>
2024-08-03 14:25:12 -0700

    added a comment

--- responsivevictory/daily-challenge/library-book-challenge/library-book-challenge.js
+++ responsivevictory/daily-challenge/library-book-challenge/library-book-challenge.js
@@ -2,6 +2,7 @@
 // Write a function that returns how many books are still checked in.
 
 const books = [true, false, true, false, true, false, true, false, true, false];
-
+//reduce uses a callback function, first paramater, and a initial value. If the initial value is true add 1 if false add 0
 let checkedInBooks = books.reduce((count, bool) => bool ? count + 1 : count, 0)
+
 console.log(`There are ${checkedInBooks} books checked in`)
