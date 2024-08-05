ec8cdb80c7b5799208155ec946d24d92e39d4b38
beau currier <beaucurrier01@gmail.com>
2024-08-04 23:24:30 -0700

    started day 12 challenge

---
+++ responsivevictory/day12/dailyChallenge/day12DailyChallenge.js
@@ -0,0 +1,34 @@
+// Challenge 1
+// Create a new object called Donis Kia
+// Donis Kia should store key value pairs of things the car could have
+// One of the keys stored should be a playlist that contains a list of Donis favorite songs
+const donisCar = {
+    brand: "Kia",
+    year: 2020,
+    occupancy: 4,
+    playlist: ["Song 1", "Song 2", "Song 3"],
+    color: "Blue",
+  };
+
+// Challenge 2
+// Create an object called "car" with properties "make", "model", "year", and "color". Use dot notation to access the value of the "year" property and console.log it.
+
+const car = {
+    make: "Toyota",
+    model: "Camry",
+    year: 2015,
+    color: "Red",
+}
+console.log(car.year)
+
+// Challenge 3
+// Create a class called "Rectangle" with properties "length" and "width". Add a method to the class called "area" that calculates and returns the area of the rectangle (length x width). Create a new instance of the class and console.log its area.
+const rectangle = {
+    length: ,
+    width:  ,
+    
+}
+
+
+// Challenge 4
+// Create a class called Employee with a constructor that takes in name and salary parameters. Add a getSalary() method that returns the employee's salary. Create a subclass called Managerthat extends Employee and has an additional bonusproperty. Override the getSalary() method in Manager to return the employee's salary plus the bonus. Create an instance of Manager with name "John" and salary 5000, and bonus1000, and call the getSalary() method.
